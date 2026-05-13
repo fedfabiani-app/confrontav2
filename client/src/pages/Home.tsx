621d6575133ecd7e6b45beb39535d950dec19b5a
Federico <fed.fabiani@gmail.com>
2026-05-13 17:02:56 +0200

    10px padding banner upgrade

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -311,7 +311,7 @@ export default function Home() {
           </div>
         </div>
 
-         <UpgradeBanner context="history" style={{ marginBottom: 10 }} />
+         <UpgradeBanner context="history" style={{ marginBottom: 10px }} />
 
         {/* Favorites Section */}
         {hasFavorites && (
