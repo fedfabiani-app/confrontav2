63213e94ec6b43ee0940483a22987c7ed5856686
Federico <fed.fabiani@gmail.com>
2026-05-14 10:19:47 +0200

    tolte " superquote

--- client/src/pages/SignDetail.tsx
+++ client/src/pages/SignDetail.tsx
@@ -898,7 +898,7 @@ function SignDetail({ sign }: SignDetailProps) {
                     <>
                       <div className="px-4 py-3 bg-gradient-to-r from-amber-50 to-yellow-50 dark:from-amber-950/30 dark:to-yellow-950/30 border-l-4" style={{ borderLeftColor: '#E1B64E' }}>
                         <p className="text-sm font-italic text-gray-800 dark:text-gray-200 italic">
-                          "{horoscope.superquote}"
+                          {horoscope.superquote}
                         </p>
                       </div>
                       <div className="border-t border-gray-200 dark:border-gray-700 my-3"></div>
