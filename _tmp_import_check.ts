f7c572bfc00aca09fe91c125722bcb398fcdc09e
federico <fed.fabiani@gmail.com>
2026-07-17 15:19:41 +0200

    fix attached assets

--- _tmp_import_check.ts
+++
@@ -1,4 +0,0 @@
-import * as claudeModule from './server/services/claude';
-import * as jobsModule from './server/jobs/index';
-console.log('claude exports:', Object.keys(claudeModule));
-console.log('jobs exports ok:', typeof jobsModule.enqueueAggregatedNlpJob === 'function' && typeof jobsModule.enqueueAggregatedWeeklyNlpJob === 'function');
